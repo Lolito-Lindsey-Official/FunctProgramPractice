1df5e5f46964425f87a2e3d4350d3588c85597f1
lolito-lindsey-official <lolitolindsey@gmail.com>
2024-04-10 10:20:32 -0500

    added miles to kilometers example

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // build a function converting fahrenheit to celsius
-let farenheit = 32;
-
 const convertToCelsius = (farenheit) => {
     return (farenheit - 32) / 1.8;
 }
 
-console.log(convertToCelsius(32));
+console.log(convertToCelsius(55));
 console.log(convertToCelsius(72));
+
+// build a function converting miles to kilometers
+let mile = 0;
+ 
+const convertToKm = (mile) => {
+    return mile * 0.62;
+}
+
+console.log(convertToKm(5));
+console.log(convertToKm(10));
+
+
